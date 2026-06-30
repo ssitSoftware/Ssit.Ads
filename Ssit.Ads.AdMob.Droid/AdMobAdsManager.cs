d219d4a3781e457e88d7eb08cf089c674e913de4
Sebastian Sejud <ssejud@gmail.com>
2026-06-30 07:13:09 +0200

    Moved  back to nested classes

--- Ssit.Ads.AdMob.Droid/AdMobAdsManager.cs
+++ Ssit.Ads.AdMob.Droid/AdMobAdsManager.cs
@@ -1,4 +1,6 @@
+using Android.Runtime;
 using Google.Android.Gms.Ads;
+using Google.Android.Gms.Ads.Initialization;
 using Google.Android.Gms.Ads.Interstitial;
 using Ssit.Ads.Core;
 
@@ -6,6 +8,74 @@ namespace Ssit.Ads.AdMob.Droid;
 
 public class AdMobAdsManager : IAdsManager
 {
+    private sealed class FullScreenCallback : FullScreenContentCallback
+    {
+        private readonly Action _onDismiss;
+        private readonly Action _onFail;
+
+        public FullScreenCallback(Action onDismiss, Action onFail)
+        {
+            _onDismiss = onDismiss;
+            _onFail = onFail;
+        }
+
+        public override void OnAdDismissedFullScreenContent() => _onDismiss();
+        public override void OnAdFailedToShowFullScreenContent(AdError error) => _onFail();
+    }
+    
+    private sealed class InitListener : Java.Lang.Object, IOnInitializationCompleteListener
+    {
+        private readonly Action _onComplete;
+        public InitListener(Action onComplete) => _onComplete = onComplete;
+        public void OnInitializationComplete(IInitializationStatus p0) => _onComplete();
+    }
+    
+    private abstract class InterstitialCallback : InterstitialAdLoadCallback  
+    {  
+        [Register("onAdLoaded", "(Lcom/google/android/gms/ads/interstitial/InterstitialAd;)V", "GetOnAdLoadedHandler")]  
+        public virtual void OnAdLoaded(InterstitialAd interstitialAd)  
+        {  
+        }  
+     
+        private static Delegate cb_onAdLoaded;  
+        private static Delegate GetOnAdLoadedHandler()  
+        {  
+            if (cb_onAdLoaded is null)  
+                cb_onAdLoaded = JNINativeWrapper.CreateDelegate((Action<IntPtr, IntPtr, IntPtr>)n_onAdLoaded);  
+            return cb_onAdLoaded;  
+        }  
+        private static void n_onAdLoaded(IntPtr jnienv, IntPtr native__this, IntPtr native_p0)  
+        {  
+            InterstitialCallback thisobject = GetObject<InterstitialCallback>(jnienv, native__this, JniHandleOwnership.DoNotTransfer);  
+            InterstitialAd resultobject = GetObject<InterstitialAd>(native_p0, JniHandleOwnership.DoNotTransfer);  
+            thisobject.OnAdLoaded(resultobject);  
+        }  
+    }
+    
+    private class LoadCallback : InterstitialCallback
+    {
+        private readonly Action<InterstitialAd> _onLoaded;
+        private readonly Action _onFailedToLoad;
+
+        public LoadCallback(Action<InterstitialAd> onLoaded, Action onFailedToLoad = null)
+        {
+            _onLoaded = onLoaded;
+            _onFailedToLoad = onFailedToLoad;
+        }
+
+        public override void OnAdFailedToLoad(LoadAdError p0)
+        {
+            base.OnAdFailedToLoad(p0);
+            _onFailedToLoad?.Invoke();
+        }
+
+        public override void OnAdLoaded(InterstitialAd interstitialAd)
+        {
+            base.OnAdLoaded(interstitialAd);
+            _onLoaded?.Invoke(interstitialAd);
+        }
+    }
+    
     public class Configuration
     {
         public string AdUnitId = "ca-app-pub-3940256099942544/1033173712";
